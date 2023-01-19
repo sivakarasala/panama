85881cb279b968af79e536f86c4c1ce8757ada44
SivaKrishna <shivataraya@gmail.com>
2023-01-19 07:27:41 +0530

    basic impl of mpsc channel

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+use std::collections::VecDeque;
+use std::sync::{Arc, Condvar, Mutex};
+
+pub struct Sender<T> {
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Sender {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+impl<T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(t);
+        drop(queue);
+        self.inner.available.notify_one();
+    }
+}
+
+pub struct Receiver<T> {
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        loop {
+            let mut queue = self.inner.queue.lock().unwrap();
+            match queue.pop_front() {
+                Some(t) => return t,
+                None => {
+                    self.inner.available.wait(queue).unwrap();
+                }
+            }
+        }
+    }
+}
+
+struct Inner<T> {
+    queue: Mutex<VecDeque<T>>,
+    available: Condvar,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Inner {
+        queue: Mutex::default(),
+        available: Condvar::new(),
+    };
+    let inner = Arc::new(inner);
+    (
+        Sender {
+            inner: inner.clone(),
+        },
+        Receiver {
+            inner: inner.clone(),
+        },
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ping_pong() {
+        let (mut tx, mut rx) = channel();
+        tx.send(42);
+        assert_eq!(rx.recv(), 42);
+    }
+}
